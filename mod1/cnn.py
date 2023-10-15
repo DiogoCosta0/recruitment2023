de134049e417d4daf47f1af2838aa3ffb76c1132
Diogo Costa <2201720@my.ipleiria.pt>
2023-10-15 21:58:59 +0100

    .docx com as respostas, Dataset.py com os models e cnn.py atualizado

--- mod1/cnn.py
+++ mod1/cnn.py
@@ -1,4 +1,5 @@
-from torch import nn, functional as F
+import torch.nn as nn
+import torch.nn.functional as F
 
 class CNN(nn.Module):
 
@@ -6,7 +7,33 @@ class CNN(nn.Module):
 
         super().__init__()
 
-        raise NotImplementedError("Define the layers here!")
+        # Convolutional layers
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+
+        # Fully connected layers
+        self.fc1 = nn.Linear(256, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+
+        #raise NotImplementedError("Define the layers here!")
 
     def forward(self, x):
-        raise NotImplementedError("Implement the forward pass here!")
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2)
+
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2)
+
+        x = x.view(-1, 256)  # Flatten the tensor
+
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+
+        x = self.fc3(x)
+
+        return x
+
+
+        #raise NotImplementedError("Implement the forward pass here!")
